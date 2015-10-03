36545f54b9d427fc0a60809be406ee96163f1a26
pwwu <1239818707@qq.com>
2015-10-04 02:17:17 +0800

    imseg modified, using the load_supp method

--- Function_Segmentation/imseg.m
+++ Function_Segmentation/imseg.m
@@ -2,7 +2,7 @@ function [imgTemplate, imgTemplateFlat] = imseg(img, method, ParaSeg)
     % Segmentation program for medical image.
     % By Pengwei Wu.
 
-    if(method == 'load')
+    if(strcmp(method, 'load'))
         dir = ParaSeg.dir;
         name = ParaSeg.name;
         nameFlat = ParaSeg.nameFlat;
@@ -16,6 +16,22 @@ function [imgTemplate, imgTemplateFlat] = imseg(img, method, ParaSeg)
         end
     end
 
+    if(strcmp(method, 'load_supp'))
+        dir = ParaSeg.dir;
+        name = ParaSeg.name;
+        nameFlat = ParaSeg.nameFlat;
+        load(strcat(dir, name));
+        load(strcat(dir, nameFlat));
+        if(~exist('imgTemplate', 'var'))
+            error('The name of the loaded template has to be imgTemplae');
+        end
+        if(~exist('imgTemplateFlat', 'var'))
+            warning(['You should also provide imgTemplateFlat']);
+        end
+        logAir = img > (ParaSeg.suppThres);
+        imgTemplate(logAir) = img(logAir) + ParaSeg.suppAdd;
+        imgTemplateFlat(logAir) = img(logAir) + ParaSeg.suppAdd;
+    end
 
 
 
